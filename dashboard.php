4756aa08757921bad5a2784155984c3843e11faa
anjaniii1701 <annlestiawan@gmail.com>
2026-04-30 13:30:09 +0700

    update dashboard

--- dashboard.php
+++ dashboard.php
@@ -6,11 +6,45 @@
     <title>Document</title>
 </head>
 <body>
-    <ul>
-        <li><a href="dashboard.php?page=alat">Data Alat</a></li>
-        <li><a href="dashboard.php?page=member">Data Member</a></li>
+<ul>
+        <li><a href="dashboard.php?page=dashboard">Dashboard</a></li>
+        <li><a href="dashboard.php?page=manajemenanggota">Manajemen Anggota</a></li>
         <li>Data Peminjaman</li>
 </ul>
+<hr>
+<nav>
+<ul>
+        <li>Dashbord</li>
+        <li>Manajemen Anggota</li>
+        <li>Laporan & Statistik</li>
+        <li>Scanner kehadiran</li>
+        <li>Log Out</li>
+</ul>
+</nav>
+</hr>
+<main>
+        <h2>Dashbord</h2>
+        <section>
+            <div>
+                <p>[Ikon Manajemen]</p>
+                <h3>Manajemen Anggota</h3>
+</div>
+<div>
+    <p>[Ikon Scanner]</p>
+    <h3>Scanner Kehadiran</h3>
+</div>
+<div>
+    <p>[Ikon Laporan]</p>
+    <h3>Laporan & Statistik</h3>
+</div>
+<div>
+    <p>[Ikon Keluar]</p>
+    <h3>Log Out</h3>
+</div>
+</section>
+</main>
+</hr>
+
 </body>
 </html>
 
--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,3 @@
 <?php
-$koneksi=mysqli_connect("localhost","root","root","LATIHAN2");
+$koneksi=mysqli_connect("localhost","root","","LATIHAN2");
 ?>
